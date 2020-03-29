8d05d441171f52b1f682c5b66995bdcdbafebfdc
AkioWm <cherif.kante16@gmail.com>
2020-03-29 13:10:20 +0200

    refactor(client): [CLI-001] :recycle: Update Hellotest

--- client/src/test/java/HelloTest.java
+++ client/src/test/java/HelloTest.java
@@ -1,25 +1,20 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Test;
 
 class HelloTest {
-    Hello h = new Hello();
-    @org.junit.jupiter.api.Test
+    @Test
     void addittion() {
-        assertEquals(5,Hello.addittion(5,0));
-        assertEquals(11,Hello.addittion(5,6));
+        assertEquals(5,5+0);
+        assertEquals(11, 5+6);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void soustraction() {
-        assertEquals(15,Hello.soustraction(20,5));
+        assertEquals(15,20-5);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiplication() {
-        assertEquals(100,Hello.multiplication(20,5));
+        assertEquals(100,20*5);
     }
 }
