611272e8e68148630cc9ea87a2b5c6c25603fe48
Loïc Lamarque <loiclamarque777@gmail.com>
2020-05-05 11:20:25 +0200

    feature(api): [API-009] :sparkles: Add not implemented routes for admins

--- api/src/routes/admin.router.js
+++ api/src/routes/admin.router.js
@@ -1,11 +1,40 @@
 const bodyParser = require('body-parser');
 
+const UserStatusMiddleware = require('../middlewares').UserStatusMiddleware;
+const HttpCodeUtil = require('../utils').HttpCodeUtil;
+
+const routes = {
+    Admins: '/admins',
+    AdminsId: '/admins/:id'
+};
+
 module.exports = (app) => {
 
-    // GET      /admins     ===> Get all administrators
-    // POST     /admins     ===> Create one administrator
+    app.use(UserStatusMiddleware.checkStatusForAdmins());
+
     // GET      /admins/:id ===> Get one administrator from id
+    app.get(routes.AdminsId, async (req, res, next) => { // TODO: not implemented!
+        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    });
+
     // DELETE   /admins/:id ===> Delete one administrator from id
-    // UPDATE   /admins/:id ===> Update one administrator from id
+    app.delete(routes.AdminsId, async (req, res, next) => { // TODO: not implemented!
+        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    });
+
+    // PUT   /admins/:id ===> Update one administrator from id
+    app.put(routes.AdminsId, async (req, res, next) => { // TODO: not implemented!
+        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    });
+
+    // GET      /admins     ===> Get all administrators
+    app.get(routes.Admins, async (req, res, next) => { // TODO: not implemented!
+        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    });
+
+    // POST     /admins     ===> Create one administrator
+    app.post(routes.Admins, async (req, res, next) => { // TODO: not implemented!
+        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    });
 
 };
