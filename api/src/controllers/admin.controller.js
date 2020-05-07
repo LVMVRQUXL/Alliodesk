ae27aaaa4160204362bea99e63caf1081a549de5
Loïc Lamarque <loiclamarque777@gmail.com>
2020-05-07 12:29:29 +0200

    feature(api): [API-009] :sparkles: Implement admin's creation

--- api/src/controllers/admin.controller.js
+++ api/src/controllers/admin.controller.js
@@ -1,9 +1,53 @@
-const {User} = require('../models');
+const UserService = require('../services').UserService;
 const UserStatusController = require('./user_status.controller');
 const SecurityUtil = require('../utils').SecurityUtil;
 
 class AdminController {
+    /**
+     * Create a new administrator
+     *
+     * @param name {string}
+     * @param email {string}
+     * @param login {string}
+     * @param password {string}
+     *
+     * @returns {Promise<boolean>}
+     */
+    async createAdmin(name, email, login, password) { // TODO: unit tests!
+        if (await this.findOneAdminFromEmail(email) || await this.findOneAdminFromLogin(login)) {
+            return false;
+        }
+        return await UserService.create(await _getAdminStatusId({
+            name: name,
+            email: email,
+            login: login,
+            password: SecurityUtil.hash(password)
+        }));
+    }
 
+    /**
+     * Find one admin from login
+     *
+     * @param login {string}
+     *
+     * @returns {Promise<UserDTO | null>}
+     */
+    async findOneAdminFromLogin(login) {
+        const user = await UserService.findOne(await _getAdminStatusId({ login: login }));
+        return !user ? null : UserService.mapToDTO(user);
+    }
+
+    /**
+     * Find one admin from email
+     *
+     * @param email {string}
+     *
+     * @returns {Promise<UserDTO | null>}
+     */
+    async findOneAdminFromEmail(email) {
+        const user = await UserService.findOne(await _getAdminStatusId({ email: email }));
+        return !user ? null : UserService.mapToDTO(user);
+    }
 }
 
 /**
--- api/src/routes/admin.router.js
+++ api/src/routes/admin.router.js
@@ -3,6 +3,7 @@ const emailValidator = require('email-validator');
 
 const UserStatusMiddleware = require('../middlewares').UserStatusMiddleware;
 const HttpCodeUtil = require('../utils').HttpCodeUtil;
+const AdminController = require('../controllers').AdminController;
 
 const routes = {
     AdminsId: '/admins/:id',
@@ -33,9 +34,61 @@ module.exports = (app) => {
         res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
     });
 
-    // POST     /admins     ===> Create one administrator
-    app.post(routes.Admins, async (req, res) => { // TODO: not implemented!
-        res.status(HttpCodeUtil.NOT_IMPLEMENTED).end();
+    /**
+     * @swagger
+     *
+     * '/admins':
+     *   post:
+     *     description: "Create one administrator"
+     *     tags:
+     *       - admins
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: name
+     *         description: "Administrator's name"
+     *         in: body
+     *         required: true
+     *       - name: email
+     *         description: "Administrator's email"
+     *         in: body
+     *         required: true
+     *       - name: login
+     *         description: "Administrator's login"
+     *         in: body
+     *         required: true
+     *       - name: password
+     *         description: "Administrator's password"
+     *         in: body
+     *         required: true
+     *     responses:
+     *       201:
+     *         description: "Created"
+     *       409:
+     *         description: "Administrator is already existing"
+     *       400:
+     *         description: "Invalid inputs"
+     *       500:
+     *         description: "An internal error has occurred"
+     */
+    app.post(routes.Admins, bodyParser.json(), async (req, res) => { // TODO: integration tests!
+        try {
+            const adminName = req.body.name;
+            const adminEmail = req.body.email;
+            const adminLogin = req.body.login;
+            const adminPassword = req.body.password;
+            if (adminName && adminName !== ""
+                && emailValidator.validate(adminEmail)
+                && adminLogin && adminLogin !== ""
+                && adminPassword && adminPassword !== "") {
+                const result = await AdminController.createAdmin(adminName, adminEmail, adminLogin, adminPassword);
+                if (result) { res.status(HttpCodeUtil.CREATED).end(); }
+                else { res.status(HttpCodeUtil.CONFLICT).end(); }
+            } else { res.status(HttpCodeUtil.BAD_REQUEST).end(); }
+        } catch (e) {
+            console.error(e);
+            res.status(HttpCodeUtil.INTERNAL_SERVER_ERROR).end();
+        }
     });
 
 };
