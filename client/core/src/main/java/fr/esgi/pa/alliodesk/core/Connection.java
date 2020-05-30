c5388eb05f3a50e3d15f0c113a3ff6375791a827
Hervé Huang <herve.huang@yahoo.fr>
2020-05-31 00:54:39 +0200

    chore(client): [CLI-010] :construction: add store token in file function

--- client/core/src/main/java/fr/esgi/pa/alliodesk/core/Connection.java
+++ client/core/src/main/java/fr/esgi/pa/alliodesk/core/Connection.java
@@ -2,16 +2,22 @@ package fr.esgi.pa.alliodesk.core;
 
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Connection {
     private InfoInForm connectionForm;
+    private String tok;
     public Connection(String login, String pwd) {
         this.connectionForm = new InfoInForm.FormBuilder()
                 .withLogin(login)
@@ -30,7 +36,24 @@ public class Connection {
         post.setEntity(postingString);
         post.setHeader("Content-type", "application/json");
         HttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            String jsonContent = EntityUtils.toString(response.getEntity());
+            JsonObject myObjectGson = new Gson().fromJson(jsonContent, JsonObject.class);
+            String token = myObjectGson.get("token_session").toString();
+            token = token.substring(1,token.length()-1);
+            AddTokenFile(token);
+        }
+
         return response.getStatusLine().getStatusCode();
+    }
+
+    public void AddTokenFile(String token) throws IOException {
+        File f = new File("Token.txt");
+        if(!f.exists()){
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Token.txt"));
+            writer.write(token);
+            writer.close();
+        }
     }
 }
 
