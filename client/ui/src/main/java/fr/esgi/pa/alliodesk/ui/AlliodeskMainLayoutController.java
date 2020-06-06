a6369b0b8084f9db92edfd3c2b0d0ffd40bf5f1d
AkioWm <cherif.kante16@gmail.com>
2020-06-06 11:56:46 +0200

    feat(client): [CLI-014] :sparkles: Update AlliodeskMainLayoutController

--- client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/AlliodeskMainLayoutController.java
+++ client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/AlliodeskMainLayoutController.java
@@ -1,10 +1,19 @@
 package fr.esgi.pa.alliodesk.ui;
 
+import fr.esgi.pa.alliodesk.ui.plugin.PluginGuetter;
+import interfacetest.PluginInterface;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 
 
 public class AlliodeskMainLayoutController {
@@ -12,16 +21,34 @@ public class AlliodeskMainLayoutController {
     private BorderPane rootLayout;
     @FXML
     private Button todolistButton;
-
+    @FXML
+    private MenuBar menu;
     @FXML
     private Button RegisterButton;
-
+    @FXML
+    private Menu pluginMenu;
     @FXML
     void showRegisterEvent(ActionEvent event) throws IOException {
         AlliodeskMainLayout.showRegisterLayout();
-
     }
 
+    @FXML
+    void addPluginInMenuBar(PluginInterface[] pi){
+        int index = 0;
+        pluginMenu.getItems().clear();
+        for( PluginInterface plugin: pi){
+            MenuItem mi = new MenuItem(plugin.getName());
+            int finalIndex = index;
+            mi.setOnAction(event -> PluginGuetter.pluginList[finalIndex].Print());
+            pluginMenu.getItems().add(mi);
+            index++;
+        }
+    }
+    @FXML
+    void loadPlugin(ActionEvent event) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        PluginGuetter.load();
+        addPluginInMenuBar(PluginGuetter.pluginList);
+    }
     @FXML
     void showTodoEvent(ActionEvent event) throws IOException {
         AlliodeskMainLayout.showToDoListLayout();
