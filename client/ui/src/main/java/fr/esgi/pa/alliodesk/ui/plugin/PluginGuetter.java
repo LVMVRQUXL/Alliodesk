dfbb22a77a4489385db18244b0b923e2ab473c3d
AkioWm <cherif.kante16@gmail.com>
2020-06-06 11:55:45 +0200

    feat(client): [CLI-014] :sparkles: Update PluginGuetter

--- client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/plugin/PluginGuetter.java
+++ client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/plugin/PluginGuetter.java
@@ -1,4 +1,20 @@
 package fr.esgi.pa.alliodesk.ui.plugin;
 
+
+import interfacetest.PluginInterface;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class PluginGuetter {
+    final static String jarPath = "/lib/plugins";
+    public static PluginInterface[] pluginList;
+    public static void load() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        PluginLoader<PluginInterface> loader = new PluginLoader<>();
+        pluginList = new PluginInterface[]{};
+        pluginList = loader.LoadClass(jarPath, "testPlugin", PluginInterface.class);
+
+        for( PluginInterface pi : pluginList){
+            pi.Print();
+        }
+    }
 }
