4e167b4473c686601fdc19fb7a0aacd436f1ab76
Loïc Lamarque <loiclamarque777@gmail.com>
2020-06-11 23:15:47 +0200

    feature(frontend): [FRO-007] :sparkles: Add toolbar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {MatSliderModule} from "@angular/material/slider";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToolbarComponent} from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
