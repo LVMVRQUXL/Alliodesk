0a865d35a2fb138b5c56a2f1d4299de887d882c1
Loïc Lamarque <loiclamarque777@gmail.com>
2020-06-12 18:56:55 +0200

    refactor(frontend): [FRO-007] :recycle: Move toolbar component in shared module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {MatToolbarModule} from "@angular/material/toolbar";
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToolbarComponent} from './toolbar/toolbar.component';
+import {SharedComponentsModule} from "./shared/components/shared-components.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ToolbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    SharedComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/shared/components/shared-components.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+import {ToolbarComponent} from "./toolbar/toolbar.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+  declarations: [
+    ToolbarComponent
+  ],
+  exports: [
+    ToolbarComponent
+  ]
+})
+export class SharedComponentsModule {
+}
