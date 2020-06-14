a94a7f5a84fceb566817ecfc7c328e69cf386787
Hervé Huang <herve.huang@yahoo.fr>
2020-06-14 04:06:41 +0200

    chore(client): [CLI-017] :construction: create controller for WScreation - delete - update view

---
+++ client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/controller/WSController.java
@@ -0,0 +1,89 @@
+package fr.esgi.pa.alliodesk.ui.controller;
+
+import fr.esgi.pa.alliodesk.core.request.WorkspaceManager;
+import fr.esgi.pa.alliodesk.ui.AlliodeskMainLayoutController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
+
+public class WSController {
+
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField description;
+
+    public void  resetFields(ActionEvent actionEvent) {
+        this.name.setText("");
+        this.description.setText("");
+    }
+
+    @FXML
+    public void createWS(ActionEvent actionEvent) {
+        WorkspaceManager wSM = new WorkspaceManager("create",name.getText(),description.getText());
+        int status_code = wSM.requestToServe();
+        switch (status_code) {
+            case 201:
+                System.out.println("Created");
+                break;
+            case 400:
+                System.out.println("Invalid workspace's name or description");
+                break;
+            case 401:
+                System.out.println("Token Unauthorized");
+                break;
+            case 500:
+                System.out.println("An internal error has occurred");
+                break;
+            default:
+                System.out.println("status code = " + status_code);
+                break;
+        }
+    }
+    public ArrayList<String[]> findAllUserWS() {
+        WorkspaceManager wSM = new WorkspaceManager("findAllUserWS",null,null);
+        int status_code = wSM.requestToServe();
+        switch (status_code) {
+            case 200:
+                System.out.println("Ok");
+                return wSM.getExistedWS();
+            case 204:
+                System.out.println("No workspaces to return");
+                return null;
+            case 400:
+                System.out.println("Invalid user's id");
+                return null;
+            case 404:
+                System.out.println("Can't find user from id");
+                return null;
+            case 500:
+                System.out.println("An internal error has occurred");
+                return null;
+            default:
+                System.out.println("status code = " + status_code);
+                return null;
+        }
+    }
+    public void removeWSFromId(){
+        WorkspaceManager wSM = new WorkspaceManager("removeWSFormId",null,null);
+        int status_code = wSM.requestToServe();
+        switch (status_code) {
+            case 200:
+                System.out.println("Ok");
+            case 204:
+                System.out.println("No workspaces to return");
+            case 400:
+                System.out.println("Invalid user's id");
+            case 404:
+                System.out.println("Can't find user from id");
+            case 500:
+                System.out.println("An internal error has occurred");
+            default:
+                System.out.println("status code = " + status_code);
+        }
+    }
+}
