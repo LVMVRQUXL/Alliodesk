817bc92221f2a257d494364eb5a7ccf06eb23193
AkioWm <cherif.kante16@gmail.com>
2020-06-19 20:51:00 +0200

    feat(client): [CLI-020] :sparkles: Add ServiceRequest

---
+++ client/core/src/main/java/fr/esgi/pa/alliodesk/core/request/ServiceRequest.java
@@ -0,0 +1,95 @@
+package fr.esgi.pa.alliodesk.core.request;
+
+import com.google.gson.Gson;
+import fr.esgi.pa.alliodesk.core.InfoInForm;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ServiceRequest extends ApiRequest {
+    private final InfoInForm srFrom;
+    private final String functionCall;
+
+    public ArrayList<Service> getExistedService() {
+        return existedService;
+    }
+
+    private ArrayList<Service> existedService = new ArrayList<>();
+
+    public ServiceRequest(String functionCall, String id) {
+        this.functionCall = functionCall;
+        this.srFrom = InfoInForm.build()
+                .withId(id);
+    }
+
+    @Override
+    public int requestToServe() {
+        switch (this.functionCall) {
+
+            case "findUserAllServices":
+                try {
+                    String temp_id = "-1";
+                    GetUserData myUser = new GetUserData();
+                    myUser.requestToServe();
+                    if (myUser.idNotEmpty()) {
+                        temp_id = myUser.getId();
+                    }
+                    final CloseableHttpResponse response = super.request(
+                            "/users/" + temp_id + "/services",
+                            new HttpGet(),
+                            this.srFrom
+                    );
+                    final int statusCode = response.getStatusLine().getStatusCode();
+
+                    if (statusCode == 200) {
+                        final String responseContent = EntityUtils.toString(response.getEntity());
+                        Service[] yourList = new Gson().fromJson(responseContent, Service[].class);
+                        existedService.addAll(Arrays.asList(yourList));
+                    }
+                    return statusCode;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return 500;
+                }
+        }
+        return 0;
+    }
+
+    public static class Service {
+        String id, name, version, source_url, user_id, service_status_id;
+
+
+        public Service(String id, String name, String version, String source_url, String user_id, String service_status_id) {
+            this.id = id;
+            this.name = name;
+            this.version = version;
+            this.source_url = source_url;
+            this.user_id = user_id;
+            this.service_status_id = service_status_id;
+        }
+
+        @Override
+        public String toString() {
+            return "Service{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", version='" + version + '\'' +
+                    ", source_url='" + source_url + '\'' +
+                    ", user_id='" + user_id + '\'' +
+                    ", service_status_id='" + service_status_id + '\'' +
+                    '}';
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+}
