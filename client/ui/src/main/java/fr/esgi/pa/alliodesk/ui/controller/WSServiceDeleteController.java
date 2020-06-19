55a7a5ca7a92f1ea66ddbf76f8a8e4fdf7913471
AkioWm <cherif.kante16@gmail.com>
2020-06-20 01:01:55 +0200

    feat(client): [CLI-019] :sparkles: Update controller

--- client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/controller/WSServiceDeleteController.java
+++ client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/controller/WSServiceDeleteController.java
@@ -2,51 +2,65 @@ package fr.esgi.pa.alliodesk.ui.controller;
 
 import fr.esgi.pa.alliodesk.core.request.ServiceRequest;
 import fr.esgi.pa.alliodesk.core.request.WorkspaceManager;
+import fr.esgi.pa.alliodesk.ui.AlliodeskMainLayoutController;
+import fr.esgi.pa.alliodesk.ui.ChoiceBoxItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
-
+import javafx.scene.control.Label;
+import static javafx.scene.paint.Color.*;
 import java.util.ArrayList;
 
 public class WSServiceDeleteController {
 
-    private ArrayList<ServiceRequest.Service> myList;
-    @FXML
-    public ChoiceBox<ServiceRequest.Service> wSList;
 
+    private String workspaceId = AlliodeskMainLayoutController.getWorkspaceId();
+    @FXML
+    public ChoiceBox<ChoiceBoxItem> servicesList;
+    @FXML
+    private Label deleteLabel;
     @FXML
     public void initialize() {
-        WSController wSC = new WSController();
-        this.myList = wSC.findAllServiceWorkspace("1");
-        ObservableList<ServiceRequest.Service> allMyWS = FXCollections.observableArrayList();
-        allMyWS.addAll(this.myList);
-        this.wSList.setItems(allMyWS);
+        ArrayList<ServiceRequest.Service> myServices = WSController.findAllServiceWorkspace(workspaceId);
+        ObservableList<ChoiceBoxItem> allMyWS = FXCollections.observableArrayList();
+        for (ServiceRequest.Service tab : myServices) {
+            ChoiceBoxItem item = new ChoiceBoxItem(tab.getId(), tab.getName());
+            allMyWS.add(item);
+        }
+        this.servicesList.setItems(allMyWS);
     }
 
     @FXML
     void deleteWorkspaceServiceUsingId(ActionEvent event) {
-        WorkspaceManager wSM = new WorkspaceManager("deleteServiceFromWorkspace", null, null, "1", wSList.getValue().getId());
+        String debug = servicesList.getValue().getId();
+        WorkspaceManager wSM = new WorkspaceManager("deleteServiceFromWorkspace", null, null, workspaceId, debug );
         int status_code = wSM.requestToServe();
         switch (status_code) {
             case 200:
-                System.out.println("Ok");
+                System.out.println("Service successfully delete");
+                servicesList.getItems().remove(servicesList.getValue());
                 break;
             case 204:
-                System.out.println("No services to return");
+                deleteLabel.setTextFill(WHITE);
+                deleteLabel.setText("No services to return");
                 break;
             case 400:
-                System.out.println("Invalid workspace id");
+                deleteLabel.setTextFill(WHITE);
+                deleteLabel.setText("Invalid inputs");
                 break;
             case 404:
-                System.out.println("Can't find workspace from id");
+                deleteLabel.setTextFill(WHITE);
+                deleteLabel.setText("Can't find workspace or service from id");
                 break;
             case 500:
-                System.out.println("An internal error has occurred");
+                deleteLabel.setTextFill(WHITE);
+                deleteLabel.setText("An internal error has occurred");
                 break;
             default:
-                System.out.println("status code = " + status_code);
+                deleteLabel.setTextFill(WHITE);
+                deleteLabel.setText("status code = " + status_code);
                 break;
         }
     }
