0dd3779f04dbd0b132b2d87e18739c60f6be4c76
AkioWm <cherif.kante16@gmail.com>
2020-07-02 01:30:55 +0200

    feat(client): [CLI-011] :sparkles: add feature to add ans delete services

--- client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/controller/MarketplaceController.java
+++ client/ui/src/main/java/fr/esgi/pa/alliodesk/ui/controller/MarketplaceController.java
@@ -1,3 +1,4 @@
+
 package fr.esgi.pa.alliodesk.ui.controller;
 
 import fr.esgi.pa.alliodesk.core.request.ServiceRequest;
@@ -61,5 +62,76 @@ public class MarketplaceController {
                 System.out.println("status code = " + status_code);
                 return null;
         }
+    }
+
+    public static void addServiceToUser(String serviceId) {
+        ServiceRequest serviceRequest = new ServiceRequest("addServiceToUser", serviceId);
+        int status_code = serviceRequest.requestToServe();
+        switch(status_code) {
+            case 200:
+                System.out.println("Ok");
+                break;
+            case 400:
+                System.out.println("Invalid user id or service id\n");
+                break;
+            case 401:
+                System.out.println("Invalid user's token session\n");
+                break;
+            case 404:
+                System.out.println("Can't find user or service from id\n");
+                break;
+            case 500:
+                System.out.println("An internal error has occurred");
+                break;
+            default:
+                System.out.println("status code = " + status_code);
+        }
+
+    }
+
+    public static void deleteServiceToUser(String serviceId) {
+        ServiceRequest serviceRequest = new ServiceRequest("deleteServiceToUser", serviceId);
+        int status_code = serviceRequest.requestToServe();
+        switch(status_code) {
+            case 200:
+                System.out.println("Ok");
+                break;
+            case 400:
+                System.out.println("Invalid user id or service id\n");
+                break;
+            case 401:
+                System.out.println("Invalid user's token session\n");
+                break;
+            case 404:
+                System.out.println("Can't find user or service from id\n");
+                break;
+            case 500:
+                System.out.println("An internal error has occurred");
+                break;
+            default:
+                System.out.println("status code = " + status_code);
+        }
+
+    }
+
+    public static void setAddItem(MenuItem mi, String serviceId) {
+        mi.setOnAction((event) -> {
+            addServiceToUser(serviceId);
+            mi.setText("Delete Service");
+            setDeleteItem(mi, serviceId);
+            WorkspaceRequest wSM = new WorkspaceRequest("findAllUserWS", null, null, null, null);
+            for(String[] workspace : wSM.getExistedWS()) {
+                new WorkspaceRequest("deleteServiceFromWorkspace", null, null, workspace[0], serviceId);
+            }
+
+        });
+    }
+
+    public static void setDeleteItem(MenuItem mi, String serviceId) {
+        mi.setOnAction((event) -> {
+            deleteServiceToUser(serviceId);
+            mi.setText("Add Service");
+            setAddItem(mi, serviceId);
+        });
     }
 }
