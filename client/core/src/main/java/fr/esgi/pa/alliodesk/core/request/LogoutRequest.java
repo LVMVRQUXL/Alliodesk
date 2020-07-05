c1be47a335bab2548ce19f9156f03189118b529e
Hervé Huang <herve.huang@yahoo.fr>
2020-07-05 03:34:39 +0200

    chore(client): [CLI-023] :construction: LOGOUT FILE(2)

---
+++ client/core/src/main/java/fr/esgi/pa/alliodesk/core/request/LogoutRequest.java
@@ -0,0 +1,38 @@
+package fr.esgi.pa.alliodesk.core.request;
+
+import fr.esgi.pa.alliodesk.core.manager.TokenManager;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+
+import java.io.IOException;
+
+public class LogoutRequest extends ApiRequest{
+
+    @Override
+    public int requestToServe() {
+        try {
+            String temp_id = "-1";
+            GetUserData myid = new GetUserData();   // Rendre "myid" plus parlant ( myUser par exemple )
+            myid.requestToServe();
+            if (myid.idNotEmpty()) {
+                temp_id = myid.getId();
+            }
+
+            final CloseableHttpResponse response = super.request(
+                    "/users/"+temp_id+"/logout",
+                    new HttpPut(),
+                    null,
+                    true
+            );
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 200) {
+                TokenManager.deleteTokenFile();
+            }
+
+            return statusCode;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 500;
+        }
+    }
+}
