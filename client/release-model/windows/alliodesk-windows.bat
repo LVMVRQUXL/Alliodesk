b2f59ff6299879bf3536f1db3c4f493ea0fa06d3
Loïc Lamarque <loiclamarque777@gmail.com>
2020-07-15 15:42:29 +0200

    chore(client): [CLI-036] :hammer: Update structure of bootstrap for Windows OS

--- client/release-model/windows/alliodesk-windows.bat
+++ client/release-model/windows/alliodesk-windows.bat
@@ -3,6 +3,6 @@ title Alliodesk VERSION
 mode con cols=80 lines=16
 color 0F
 
-java -DenvFile=".env" -jar "./alliodesk-ui-<VERSION>.jar"
+java -DenvFile="./app/.env" -jar "./app/alliodesk-ui-<VERSION>.jar"
 
 pause > nul
