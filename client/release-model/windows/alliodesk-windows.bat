e33b7c2bd252e7442a382e3262cf3af048dd0827
Loïc Lamarque <loiclamarque777@gmail.com>
2020-07-17 23:40:41 +0200

    chore(client): [HFX-006] :hammer: Update Batch script for Windows OS

--- client/release-model/windows/alliodesk-windows.bat
+++ client/release-model/windows/alliodesk-windows.bat
@@ -3,6 +3,6 @@ title Alliodesk VERSION
 mode con cols=80 lines=16
 color 0F
 
-java -DenvFile="./app/.env" -jar "./app/alliodesk-ui-<VERSION>.jar"
+java -DenvFile="./.env" -jar "./app/alliodesk-ui-<VERSION>.jar"
 
 pause > nul
